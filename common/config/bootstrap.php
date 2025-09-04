033800a45a25e7a5689f73fb577a39b319f348d4
dev <dev@Devs-MacBook-Pro.local>
2025-09-04 14:27:47 +0300

    Secured Sensitive Info in .env

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,3 +3,5 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+$dotenv->load();
